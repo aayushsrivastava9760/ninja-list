32a5793fee56f40f64b7cec764c6a7ec3a0d02f3
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2021-10-30 00:23:39 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+          <h1>About</h1>
+          <p>
+              Et incididunt eiusmod consequat eiusmod ad officia voluptate laboris in velit id. Velit exercitation tempor enim dolor ipsum. Qui eu excepteur officia pariatur. Ex eu enim sit est sint incididunt eu anim non reprehenderit ex cupidatat veniam. Est reprehenderit consectetur consectetur proident est officia cillum velit ea nostrud occaecat excepteur. Et eiusmod tempor ut irure veniam excepteur esse eu. Pariatur quis tempor non sit nostrud ullamco mollit pariatur voluptate aute.
+          </p>
+          <p>
+              Et incididunt eiusmod consequat eiusmod ad officia voluptate laboris in velit id. Velit exercitation tempor enim dolor ipsum. Qui eu excepteur officia pariatur. Ex eu enim sit est sint incididunt eu anim non reprehenderit ex cupidatat veniam. Est reprehenderit consectetur consectetur proident est officia cillum velit ea nostrud occaecat excepteur. Et eiusmod tempor ut irure veniam excepteur esse eu. Pariatur quis tempor non sit nostrud ullamco mollit pariatur voluptate aute.
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default About
